5bddc7339b443527b2e9795b2c5e1ff3b06cf628
Piotr Glaza <start@Piotrs-Mac-mini.hi.link>
2022-03-23 18:55:16 +0100

    Working on front

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -11,8 +11,18 @@ class ShopController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('shop/index.html.twig', [
-            'controller_name' => 'ShopController',
-        ]);
+        return $this->render('shop/index.html.twig');
+    }
+
+    #[Route('/item-single/{id}', name:"item-single")]
+    public function itemSingle($id): Response
+    {
+        return $this->render('shop/item-single.html.twig');
+    }
+
+    #[Route('/items-category/{category}', name:"items-category")]
+    public function itemsCategory($category = 'default')
+    {
+        return $this->render('shop/items-category.html.twig');
     }
 }
